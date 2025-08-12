a42ea020800f0a6415b3ce8a86e3de74a7d8b5c2
louzada08 <valuz.anderson.to@gmail.com>
2025-08-11 21:56:18 -0300

    pasta NSE.WebApp.MVC e subpastas excluidas

--- NSE.WebApp.MVC/Services/IAuthService.cs
+++
@@ -1,7 +0,0 @@
-namespace NSE.WebApp.MVC.Services;
-
-public interface IAuthService
-{
-    // Adicione os métodos necessários para autenticação
-    // Exemplo: Task<UserResponseLogin> Login(UserLogin userLogin);
-}
